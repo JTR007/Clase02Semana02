a48b3f3a0dc037d8e8b4256ed5c8927f42c87306
JTR007 <a18009483@idat.edu.pe>
2021-09-05 23:48:26 -0500

    actualizacion

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const promptNumer = () => {
     const numer = prompt("Inserte un numero");
     if(numer > 0) {
         alert("1");
-        promptNumer();
+        
     } else if(numer < 0) {
         alert("-1")
-        promptNumer();
+        
     } else {
         alert("0");
-        promptNumer();
+        
     }
 };
 
